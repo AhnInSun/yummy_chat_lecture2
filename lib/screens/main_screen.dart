8a30d125860332a4d8388e4486c686b760c66aa4
ahninsun <informationx@naver.com>
2022-09-24 18:07:34 +0900

    welcome back

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -35,21 +35,21 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       RichText(
-                          text: const TextSpan(
-                            text: 'Welcome',
-                            style: TextStyle(
-                              letterSpacing: 1.0,
-                              fontSize: 25,
-                              color: Colors.white
+                          text: TextSpan(
+                              text: 'Welcome',
+                              style: const TextStyle(
+                                  letterSpacing: 1.0,
+                                  fontSize: 25,
+                                  color: Colors.white
                             ),
                             children: [
                               TextSpan(
-                                text: ' to Yummy chat!',
-                                style: TextStyle(
+                                text: isSignupScreen ? ' to Yummy chat!' : ' back',
+                                style: const TextStyle(
                                     letterSpacing: 1.0,
                                     fontSize: 25,
                                     color: Colors.white,
-                                  fontWeight: FontWeight.bold
+                                    fontWeight: FontWeight.bold
                                 ),
                               )
                             ]
@@ -70,6 +70,7 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                 ),
               )
           ),
+          // 배경
           Positioned(
             top: 180,
             child: Container(
@@ -146,6 +147,7 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                     ],
                   ),
                   Container(
+                    margin: const EdgeInsets.only(top: 20),
                     child: Form(
                       child: Column(
                         children: [
@@ -167,8 +169,72 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                                   ),
                                   borderRadius: BorderRadius.all(Radius.circular(35.0))
                               ),
+                              hintText: 'User name',
+                              hintStyle: TextStyle(
+                                fontSize: 14,
+                                color: Palette.textColor1
+                              ),
+                              contentPadding: EdgeInsets.all(10)
                             ),
-                          )
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.account_circle,
+                                  color: Palette.iconColor,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Palette.textColor1
+                                    ),
+                                    borderRadius: BorderRadius.all(Radius.circular(35.0))
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Palette.textColor1
+                                    ),
+                                    borderRadius: BorderRadius.all(Radius.circular(35.0))
+                                ),
+                                hintText: 'User name',
+                                hintStyle: TextStyle(
+                                    fontSize: 14,
+                                    color: Palette.textColor1
+                                ),
+                                contentPadding: EdgeInsets.all(10)
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.account_circle,
+                                  color: Palette.iconColor,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Palette.textColor1
+                                    ),
+                                    borderRadius: BorderRadius.all(Radius.circular(35.0))
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Palette.textColor1
+                                    ),
+                                    borderRadius: BorderRadius.all(Radius.circular(35.0))
+                                ),
+                                hintText: 'User name',
+                                hintStyle: TextStyle(
+                                    fontSize: 14,
+                                    color: Palette.textColor1
+                                ),
+                                contentPadding: EdgeInsets.all(10)
+                            ),
+                          ),
                         ],
                       ),
                     ),
@@ -176,7 +242,76 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                 ],
               ),
             ),
-          )
+          ),
+          // 텍스트 폼 필드
+          Positioned(
+              top: 430,
+              right: 0,
+              left: 0,
+              child: Center(
+                child: Container(
+                  padding: const EdgeInsets.all(15),
+                  height: 90,
+                  width: 90,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(50)
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        colors: [
+                          Colors.orange,
+                          Colors.red
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.3),
+                          spreadRadius: 1,
+                          blurRadius: 1,
+                          offset: const Offset(0,1)
+                        )
+                      ]
+                    ),
+                    child: const Icon(
+                      Icons.arrow_forward,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              )
+          ),
+          // 전송버튼
+          Positioned(
+              top: MediaQuery.of(context).size.height-125,
+              right: 0,
+              left: 0,
+              child: Column(
+                children: [
+                  const Text('or Signup with'),
+                  TextButton.icon(
+                      onPressed: (){
+
+                      },
+                      style: TextButton.styleFrom(
+                        primary: Colors.white,
+                        minimumSize: const Size(155, 40),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)
+                        ),
+                        backgroundColor: Palette.googleColor
+                      ),
+                      icon: const Icon(Icons.add),
+                      label: const Text('Google')
+                  ),
+                ],
+              )
+          ),
+          // 구글 회원가입
         ],
       ),
     );
